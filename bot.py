b6955e989bf31af3e4750098108b94a61bf129c4
eex9 <eex9.gs@gmail.com>
2026-01-20 21:44:41 -0500

    fix images (again)

--- bot.py
+++ bot.py
@@ -80,9 +80,9 @@ class Bot(discord.Client):
             match item.get("type", None):
                 case "message":
                     # send messages as bot
-                    if len(item.get("message")) <= 0:
-                        if len(item.get("files", [])) + len(item.get("embeds", [])) <= 0:
-                            continue
+                    # if len(item.get("message")) <= 0:
+                    #     if len(item.get("files", [])) + len(item.get("embeds", [])) <= 0:
+                    #         continue
                     if self.curr_member is None or item.get("except", False):
                         self.last_sent_message = await channel.send(item.get("message","No message provided"), embeds=item.get("embed", []), reference=item.get("reference"))
                     else:
